53fd5eab0c99fdf4f30926a07c240ae038ff3f73
Wolfof420Street <billodida420@gmail.com>
2019-07-26 15:41:16 +0300

    developing the app

--- app/src/main/java/com/wolf/tryme/MainActivity.java
+++ app/src/main/java/com/wolf/tryme/MainActivity.java
@@ -2,13 +2,44 @@ package com.wolf.tryme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private View windowView;
+    private Button tryMeButton;
+    private int[] colors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        colors = new int[] {Color.RED, Color.CYAN, Color.DKGRAY, Color.LTGRAY, Color.MAGENTA, Color.BLUE, Color.GREEN, Color.YELLOW, Color.GRAY, Color.WHITE};
+
+        windowView = findViewById(R.id.windowViewId);
+
+
+        tryMeButton = (Button) findViewById(R.id.tryMeButton);
+        tryMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int colorArrayLength = colors.length;
+
+                Random random = new Random();
+                int randomNum = random.nextInt(colorArrayLength);
+
+                windowView.setBackgroundColor(colors[randomNum]);
+
+            }
+        });
+
+
+
     }
 }
